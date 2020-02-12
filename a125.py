e63df76ad08e7d173c5dcb83d00e5a6650a302cb
xingxu <17621667827@163.com>
2020-02-12 15:29:41 +0800

    kkkk

--- a125.py
+++ a125.py
@@ -4,19 +4,6 @@
 # This module is part of GitPython and is released under
 # the BSD License: http://www.opensource.org/licenses/bsd-license.php
 
-
-
-
-
-
-
-
-
-
-
-
-
-sssss
 from collections import namedtuple
 import logging
 import os
